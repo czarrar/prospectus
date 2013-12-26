e4e1bf4609a338eaa3519157b2abfbcb56f5a0f3
Zarrar Shehzad <czarrar@gmail.com>
2013-12-25 19:37:36 -0500

    first commit of source-code for my blog

--- Gemfile
+++ Gemfile
@@ -3,7 +3,8 @@ source "https://rubygems.org"
 group :development do
   gem 'rake', '~> 0.9'
   gem 'jekyll', '~> 0.12'
-  gem 'rdiscount', '~> 2.0.7'
+  #  gem 'rdiscount', '~> 2.0.7'
+  gem 'kramdown', '~> 0.13.8'
   gem 'pygments.rb', '~> 0.3.4'
   gem 'RedCloth', '~> 4.2.9'
   gem 'haml', '~> 3.1.7'
@@ -18,3 +19,4 @@ group :development do
 end
 
 gem 'sinatra', '~> 1.4.2'
+gem 'pry'
--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/prospectus/"
+http_images_path = "/prospectus/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/prospectus/fonts"
+css_dir = "public/prospectus/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
